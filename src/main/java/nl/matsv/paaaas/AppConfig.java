b0b4396bcab47fe6199fc593461cc68d0d58d84a
Matsv <mats@matsv.nl>
2016-08-11 23:46:50 +0200

    Remove the bean factory.

--- src/main/java/nl/matsv/paaaas/AppConfig.java
+++ src/main/java/nl/matsv/paaaas/AppConfig.java
@@ -1,23 +1,13 @@
 package nl.matsv.paaaas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    private final AutowireCapableBeanFactory beanFactory;
-
-    @Autowired
-    public AppConfig(AutowireCapableBeanFactory beanFactory) {
-        this.beanFactory = beanFactory;
-    }
 
     @Bean
     public DataProvider dataProvider() {
-        DataProvider provider = new DataProvider();
-        beanFactory.autowireBean(provider);
-        return provider;
+        return new DataProvider();
     }
 }
